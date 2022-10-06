dae86860ce147f2c70f9fdfb32d9e192b957b8a1
vasiliySf <vasiliy.ts@gmail.com>
2022-10-06 22:53:39 +0300

    1

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -66,3 +66,40 @@ function filterContent(inputParseFunction){ // в этот параметр бу
         }
     }
 }
+/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+*
+* */
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+/*
+* Запишем отзыв на страницу
+*
+* */
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
